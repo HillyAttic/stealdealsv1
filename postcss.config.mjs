972db8a7b079551b5a0236698ab437fada1bd5bf
HillyAttic <ncsharmaofficial@gmail.com>
2025-05-04 16:06:18 +0530

    Fix PostCSS config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,15 +2,5 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? {
-      'cssnano': {
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: false,
-        }],
-      }
-    } : {})
   },
 };
